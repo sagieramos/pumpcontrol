fbcb0b3b3adb7440864b81c2ba22cad2700a0462
Stanley Osagie <sagiecyber@gmail.com>
2024-07-03 00:59:24 +0100

    Bug fixed

--- src/control.cpp
+++ src/control.cpp
@@ -1,16 +1,15 @@
 #include "main.h"
 #include <EEPROM.h>
 
-constexpr int FLOAT_SIGNAL_PIN = 13;  // Float signal pin
+constexpr int FLOAT_SIGNAL_PIN = 21;  // Float signal pin
 constexpr int PUMP_RELAY_PIN = 12;    // Relay pin for pump
 constexpr uint8_t MAGIC_NUMBER = 123; // Magic number for EEPROM data validity
 constexpr size_t MAGIC_NUMBER_SIZE = sizeof(MAGIC_NUMBER);
+constexpr int FLOAR_SIGNAL_TRESHOLD = 1500;
 bool signalState = false; // Flag to track signal state
 
 controlData &getControlData() {
   static controlData ctrl;
-  // Read control from EEPROM
-  EEPROM.get(MAGIC_NUMBER_SIZE, ctrl.timer);
   return ctrl;
 }
 
@@ -28,7 +27,7 @@ bool setControlData(const control &timer) {
   return EEPROM.commit();
 }
 
-void controlPump(bool state) {
+bool controlPump(bool state) {
   static bool pumpState = false; // Static variable to track current pump state
 
   // Only change state if it's different from current state
@@ -37,40 +36,44 @@ void controlPump(bool state) {
     // Turn Pump ON or OFF based on state
     digitalWrite(PUMP_RELAY_PIN, state ? HIGH : LOW);
     DEBUG_SERIAL_PRINTLN(state ? "Pump is ON" : "Pump is OFF");
+
+    return true;
   }
+
+  return false;
 }
 
 void controlPumpState() {
   controlData &ctrl = getControlData();
   switch (ctrl.mode) {
+  case MachineMode::AUTO: {
+    if (signalState) {
+      static unsigned long lastChangeTime = 0;
+      unsigned long currentTime = millis();
+      unsigned long signalTime = currentTime - lastChangeTime;
+
+      if (signalTime >= ctrl.timer.resting) {
+        lastChangeTime = currentTime;
+        // Turn Pump OFF after running
+        if (controlPump(false)) {
+          DEBUG_SERIAL_PRINTF("Pump ran in: %lums\n", signalTime);
+        }
+      } else if (signalTime >= ctrl.timer.running) {
+        // Keep Pump ON after running
+        if (controlPump(true)) {
+          DEBUG_SERIAL_PRINTF("Pump ran in: %lums\n", signalTime);
+        }
+      }
+    } else {
+      controlPump(false);
+    }
+  } break;
   case MachineMode::POWER_ON:
     controlPump(true);
     break;
   case MachineMode::POWER_OFF:
     controlPump(false);
     break;
-  case MachineMode::AUTO: {
-    static unsigned long lastChangeTime = 0;
-    unsigned long currentTime = millis();
-
-    if (signalState) {
-      // Signal is high
-      if (currentTime - lastChangeTime >= ctrl.timer.running) {
-        controlPump(false); // Turn Pump OFF after running
-        lastChangeTime = currentTime;
-      } else {
-        controlPump(true); // Keep Pump ON
-      }
-    } else {
-      // Signal is low
-      if (currentTime - lastChangeTime >= ctrl.timer.resting) {
-        controlPump(true); // Turn Pump ON after timeOff
-        lastChangeTime = currentTime;
-      } else {
-        controlPump(false); // Keep Pump OFF
-      }
-    }
-  } break;
   default:
     DEBUG_SERIAL_PRINTLN("Invalid mode");
     break;
@@ -105,6 +108,7 @@ void runMachine(void *parameter) {
   (void)parameter;
   for (;;) {
     int signal = 0;
+
     for (int i = 0; i < 5; i++) {
       signal += digitalRead(FLOAT_SIGNAL_PIN) == HIGH ? 1 : 0;
       vTaskDelay(500 / portTICK_PERIOD_MS);
@@ -112,12 +116,10 @@ void runMachine(void *parameter) {
 
     signalState = signal >= 4;
     controlPumpState();
-    
 
     UBaseType_t stackHighwater = uxTaskGetStackHighWaterMark(NULL);
     DEBUG_SERIAL_PRINTF("Float Signal Task Stack Highwater: %u\n",
                         stackHighwater);
-
 
     vTaskDelay(1000 / portTICK_PERIOD_MS);
   }
@@ -131,15 +133,8 @@ void setupPumpControl() {
   // EEPROM.begin(512); // Initialize EEPROM with size. already initialized in
   // main.cpp
 
-  uint8_t magicNumber = 0;
-
-  // Read magic number from EEPROM to check if control data is present
-  EEPROM.get(0, magicNumber);
-  bool isControlDataPresent = (magicNumber == MAGIC_NUMBER);
-
-  if (!isControlDataPresent) {
+  if (EEPROM.read(0) != MAGIC_NUMBER) {
     control timer;
-
     EEPROM.put(0, MAGIC_NUMBER);
     EEPROM.put(MAGIC_NUMBER_SIZE, timer);
     EEPROM.commit();
